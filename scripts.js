975103ad15cad1237db05096b8f081a2988e7db0
phylisha <phylisha.minor@genspark.net>
2022-06-22 20:45:04 -0500

    commit2

--- scripts.js
+++ scripts.js
@@ -2,13 +2,27 @@
 // Remember to pay attention to page loading!
 window.addEventListener("load", function () {
 
-    const takeoff = this.document.getElementById('takeoff');
-    const status = this.document.getElementById('flightStatus');
+    const takeoff = document.getElementById('takeoff');
+    const status = document.getElementById('flightStatus');
+    const sBackground = document.getElementById('shuttleBackground')
+    const sHeight = document.getElementById('spaceShuttleHeight')
+    const down = document.getElementById('down');
+    const imgObj = document.getElementById('rocket');
 
-    takeoff.addEventListener("click", function (event) {
+
+    takeoff.addEventListener("click", function () {
         let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (result) {
-            status.innerHTML('Shuttle in flight');
+            status.innerHTML = 'Shuttle in flight';
+            sBackground.style.backgroundColor = 'teal';
+            sHeight.innerHTML = 10000;
+
         }
+    })
+
+    down.addEventListener("click", function () {
+        let movement = parseInt(imgObj.style.position) + "-10px";
+        imgObj.style.bottom = movement;
+        sHeight.innerHTML = sHeight.innerHTML - 1000;
     })
 })
