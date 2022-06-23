3383a3b93f26c819703f44edb4ce73a5f1aa8b5e
phylisha <phylisha.minor@genspark.net>
2022-06-22 20:44:21 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function () {
+
+    const takeoff = this.document.getElementById('takeoff');
+    const status = this.document.getElementById('flightStatus');
+
+    takeoff.addEventListener("click", function (event) {
+        let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (result) {
+            status.innerHTML('Shuttle in flight');
+        }
+    })
+})
